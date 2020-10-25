065d3e2ebf9eebcd68f3aa95d49041ae6361bf3f
Isaac Johnson <isaacjohnsononline@gmail.com>
2020-10-26 00:50:47 +1000

    docs(junklab): junklab specific package versioning and repository setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Junklab Games`,
+    siteUrl: `https://junklabgames.github.io/junklabgames/`,
+    description: `Junklab Games - Creative indie studio specialising in fun, casual mobile games`,
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
