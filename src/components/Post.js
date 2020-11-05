32a9419e724095b4642eaafe33825d9eda79e59c
Isaac Johnson <isaacjohnsononline@gmail.com>
2020-11-06 00:34:02 +1000

    feat(post): post pop up animation on hover

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,19 +3,30 @@ import Image from 'gatsby-image';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function Post({ title, author, date, description, path, featuredImage }) {
+export default function Post({
+  title,
+  author,
+  date,
+  description,
+  path,
+  featuredImage,
+  animate = true,
+}) {
   return (
-    <article className="mr-4 mb-8 last:mr-0">
-      <Image fluid={featuredImage.childImageSharp.fluid} />
-      <header>
-        <small>
-          <time>{date}</time>
-        </small>
-        <h4>{title}</h4>
-        <a href={path} className="text-tertiary">
-          Read Post <FontAwesomeIcon icon="angle-right" />
-        </a>
-      </header>
-    </article>
+    <a href={path}>
+      <article className={`mr-4 mb-8 last:mr-0 ${animate ? 'hover-pop-up' : ''}`}>
+        <Image fluid={featuredImage.childImageSharp.fluid} />
+        <header>
+          <small>
+            <time>{date}</time>
+          </small>
+          <h4>{title}</h4>
+
+          <span className="text-tertiary">
+            Read Post <FontAwesomeIcon icon="angle-right" />
+          </span>
+        </header>
+      </article>
+    </a>
   );
 }
--- src/layout/Blog.js
+++ src/layout/Blog.js
@@ -18,8 +18,8 @@ export default function Blog({ children, title, date, more }) {
         <div className="flex flex-col lg:flex-row lg:p-20">
           <main className="lg:w-3/4 px-4 lg:pr-20 w-full prose max-w-none">{children}</main>
           <aside className="lg:w-1/4 w-full text-center">
-            <div className="shadow-lg py-5 flex items-center flex-col">
-              <h3 className="mb-4">More from the blog</h3>
+            <div className="shadow-md py-5 flex items-center flex-col">
+              <h3 className="mb-4">Fresh off the press</h3>
               <article className="w-article lg:w-full">
                 {more.edges.map((post) => {
                   const { title, date, path, featuredImage } = post.node.frontmatter;
@@ -31,6 +31,7 @@ export default function Blog({ children, title, date, more }) {
                       key={`${date}__${title}`}
                       path={path}
                       featuredImage={featuredImage}
+                      animate={false}
                     />
                   );
                 })}
